68a6752332d828e458fe58a7ada66773a8329dc4
overpas <pckeycalculator@gmail.com>
2021-11-08 21:17:50 +0300

    Collect code coverage info with kover

--- treemap-core/src/test/kotlin/by/overpass/treemapchart/core/measure/sliceanddice/SliceAndDiceMeasurerTest.kt
+++ treemap-core/src/test/kotlin/by/overpass/treemapchart/core/measure/sliceanddice/SliceAndDiceMeasurerTest.kt
@@ -7,10 +7,10 @@ import org.junit.Test
 class SliceAndDiceMeasurerTest {
 
     private val sliceAndDiceMeasurer = SliceAndDiceMeasurer()
+    private val values = listOf(6.0, 6.0, 4.0, 3.0, 2.0, 2.0, 1.0)
 
     @Test
-    fun `nodes are measured correctly the slice-and-dice way`() {
-        val values = listOf(6.0, 6.0, 4.0, 3.0, 2.0, 2.0, 1.0)
+    fun `nodes are measured correctly the slice-and-dice way (vertical)`() {
         val expectedNodes = listOf(
             TreemapNode(width = 1080, height = 480, offsetX = 0, offsetY = 0),
             TreemapNode(width = 1080, height = 480, offsetX = 0, offsetY = 480),
@@ -18,9 +18,25 @@ class SliceAndDiceMeasurerTest {
             TreemapNode(width = 1080, height = 240, offsetX = 0, offsetY = 1280),
             TreemapNode(width = 1080, height = 160, offsetX = 0, offsetY = 1520),
             TreemapNode(width = 1080, height = 160, offsetX = 0, offsetY = 1680),
-            TreemapNode(width = 1080, height = 80, offsetX = 0, offsetY = 1840)
+            TreemapNode(width = 1080, height = 80, offsetX = 0, offsetY = 1840),
         )
         val nodes = sliceAndDiceMeasurer.measureNodes(values, 1080, 1920)
+
+        assertEquals(expectedNodes, nodes)
+    }
+
+    @Test
+    fun `nodes are measured correctly the slice-and-dice way (horizontal)`() {
+        val expectedNodes = listOf(
+            TreemapNode(width = 480, height = 1080, offsetX = 0, offsetY = 0),
+            TreemapNode(width = 480, height = 1080, offsetX = 480, offsetY = 0),
+            TreemapNode(width = 320, height = 1080, offsetX = 960, offsetY = 0),
+            TreemapNode(width = 240, height = 1080, offsetX = 1280, offsetY = 0),
+            TreemapNode(width = 160, height = 1080, offsetX = 1520, offsetY = 0),
+            TreemapNode(width = 160, height = 1080, offsetX = 1680, offsetY = 0),
+            TreemapNode(width = 80, height = 1080, offsetX = 1840, offsetY = 0),
+        )
+        val nodes = sliceAndDiceMeasurer.measureNodes(values, 1920, 1080)
 
         assertEquals(expectedNodes, nodes)
     }
--- treemap-core/src/test/kotlin/by/overpass/treemapchart/core/measure/squarified/SquarifiedMeasurerTest.kt
+++ treemap-core/src/test/kotlin/by/overpass/treemapchart/core/measure/squarified/SquarifiedMeasurerTest.kt
@@ -7,10 +7,10 @@ import org.junit.Test
 class SquarifiedMeasurerTest {
 
     private val squarifiedMeasurer = SquarifiedMeasurer()
+    private val values = listOf(6.0, 6.0, 4.0, 3.0, 2.0, 2.0, 1.0)
 
     @Test
-    fun `nodes are measured according to the squarified algorithm`() {
-        val values = listOf(6.0, 6.0, 4.0, 3.0, 2.0, 2.0, 1.0)
+    fun `nodes are measured according to the squarified algorithm (vertical)`() {
         val expectedNodes = listOf(
             TreemapNode(width = 540, height = 960, offsetX = 0, offsetY = 0),
             TreemapNode(width = 540, height = 960, offsetX = 540, offsetY = 0),
@@ -18,9 +18,25 @@ class SquarifiedMeasurerTest {
             TreemapNode(width = 630, height = 411, offsetX = 0, offsetY = 1508),
             TreemapNode(width = 450, height = 384, offsetX = 630, offsetY = 960),
             TreemapNode(width = 450, height = 384, offsetX = 630, offsetY = 1344),
-            TreemapNode(width = 450, height = 192, offsetX = 630, offsetY = 1728)
+            TreemapNode(width = 450, height = 192, offsetX = 630, offsetY = 1728),
         )
         val nodes = squarifiedMeasurer.measureNodes(values, 1080, 1920)
+
+        assertEquals(expectedNodes, nodes)
+    }
+
+    @Test
+    fun `nodes are measured according to the squarified algorithm (horizontal)`() {
+        val expectedNodes = listOf(
+            TreemapNode(width = 960, height = 540, offsetX = 0, offsetY = 0),
+            TreemapNode(width = 960, height = 540, offsetX = 0, offsetY = 540),
+            TreemapNode(width = 548, height = 630, offsetX = 960, offsetY = 0),
+            TreemapNode(width = 411, height = 630, offsetX = 1508, offsetY = 0),
+            TreemapNode(width = 384, height = 450, offsetX = 960, offsetY = 630),
+            TreemapNode(width = 384, height = 450, offsetX = 1344, offsetY = 630),
+            TreemapNode(width = 192, height = 450, offsetX = 1728, offsetY = 630),
+        )
+        val nodes = squarifiedMeasurer.measureNodes(values, 1920, 1080)
 
         assertEquals(expectedNodes, nodes)
     }
